78d68d4806169ede0c830619200bd69635a18554
Kallis Muhammad Radiman <92567878+Kallis361@users.noreply.github.com>
2023-03-08 10:38:30 +0700

    pertama

---
+++ app/src/main/java/com/kallis202102243/kallismr202102243/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kallis202102243.kallismr202102243;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
